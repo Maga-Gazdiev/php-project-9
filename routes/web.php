a0880a31bb1bc647c009408eb1c80323aa30f6c5
Maga-Gazdiev <marmokanime@yandex.com>
2022-10-24 22:19:02 +0300

    push

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'App\Http\Controllers\PrServer@home')->name('/home');
+
 Route::get('/urls', 'App\Http\Controllers\PrServer@index')->name('urls.index');
+
 Route::post('/urls', 'App\Http\Controllers\PrServer@store')->name('urls.store');
+
 Route::post('/urls/{id}/checks', 'App\Http\Controllers\PrServer@checks')->name('urls.checks');
+
 Route::get('/urls/{id}', 'App\Http\Controllers\PrServer@show')->name('urls.show');
 
 
