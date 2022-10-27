35c9a766c92ad63b22409fb7674607fdea4b2f72
Maga-Gazdiev <marmokanime@yandex.com>
2022-10-27 16:21:45 +0300

    push

--- app/Http/Controllers/PrServer.php
+++ app/Http/Controllers/PrServer.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
-class UrlController extends Controller
+class PrServer extends Controller
 {
     public function index(): View
     {
