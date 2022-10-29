06f0db39bb06b9213e6d275e6e4372bcd720bab1
Maga-Gazdiev <marmokanime@yandex.com>
2022-10-29 18:01:19 +0300

    push

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,6 +4,9 @@
 @extends('layouts.layouts')
 
 @section('new_content')
+
+@include('flash::message')
+
 <div class="container-lg">
     <div class="table-responsive">
         <table class="table table-bordered table-hover text-nowrap">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
 @extends('layouts.layouts')
 
 @section('new_content')
-<div class="container">
     <div class="row mt-3">
         <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded-3 bg-light p-5">
             <h1 class="display-3">Анализатор страниц</h1>
@@ -12,10 +11,11 @@
             <form action="{{ route('urls.store') }}" method="POST"
                   class="d-flex justify-content-center">
                 @csrf
-                <input type="text" name="url[name]" value="" class="form-control form-control-lg" placeholder="https://www.example.com">
-                <input type="submit" class="btn btn-primary btn-lg ms-5 px-5 text-uppercase mx-3" value="Проверить">
+                <input type="text" name="url[name]" value="" class="form-control form-control-lg"
+                       placeholder="https://www.example.com">
+                <input type="submit" class="btn btn-primary btn-lg ms-3 px-5 text-uppercase mx-3"
+                       value="Проверить">
             </form>
         </div>
     </div>
-</div>
 @endsection
