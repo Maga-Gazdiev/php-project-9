af9f56bba44b1b85676b9fa064471f5f19b7c383
Maga-Gazdiev <marmokanime@yandex.com>
2022-10-29 20:38:48 +0300

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,13 @@
 
             <form action="{{ route('urls.store') }}" method="POST" class="d-flex justify-content-center">
                 @csrf
-                <input type="text" name="url[name]" value="" class="form-control form-control-lg is-invalid"placeholder="https://www.example.com">
-                <div class="invalid-feedback">Некорректный URL</div>
-                <input type="submit" class="btn btn-primary btn-lg ms-3 px-5 text-uppercase mx-3" value="Проверить">
+                <div class="col-8">
+                    <input type="text" name="url[name]" value="" class="form-control form-control-lg is-invalid"placeholder="https://www.example.com">
+                    <div class="invalid-feedback">Некорректный URL</div>
+                </div>
+                <div class="col-2">
+                    <input type="submit" class="btn btn-primary btn-lg ms-3 px-5 text-uppercase mx-3" value="Проверить">
+                </div>
             </form>
         </div>
     </div>
