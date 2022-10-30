07fcea0c9a554c078bf38b4d1ae492ed53de8c58
Maga-Gazdiev <marmokanime@yandex.com>
2022-10-30 13:37:05 +0300

    push

--- app/Http/Controllers/PrServer.php
+++ app/Http/Controllers/PrServer.php
@@ -23,15 +23,12 @@ class PrServer extends Controller
 
     public function store(Request $request)
     {
-        $Url = $request->input('url.name');
         $validated = Validator::make($request->all(), [
             'url.name' => 'url|required|max:255',
         ]);
-        if (substr($Url, 0, 8) !== "https://" && $validated->fails() || substr($Url, 0, 7) !== "http://" && $validated->fails() ) {
-            return redirect()->route('/home');
-        }
 
-        
+        $Url = $request->input('url.name');
+        if(substr($Url, 0, 8) == "https://" || substr($Url, 0, 7) == "http://"){
         $getNormalUrl = function($Url)
         {
           $nameUrl = mb_strtolower($Url);
@@ -56,6 +53,10 @@ class PrServer extends Controller
         flash('Страница успешно добавлена')->success();
         $id = DB::table('urls')->where('name', $name)->value('id');
         return redirect()->route('urls.show', $id);
+        } elseif ($validated->fails()) {
+            flash('Некорректный URL')->error();
+            return redirect()->route('main')->withErrors($validated);
+        }
     }  
 
     public function checks(Request $request, $id)
