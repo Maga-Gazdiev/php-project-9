cd05cd4f8335fbf574b48f047df36825a78e3938
Maga-Gazdiev <marmokanime@yandex.com>
2022-10-30 13:48:10 +0300

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
 @extends('layouts.layouts')
 
 @section('new_content')
+
+@include('flash::message')
+<div class="container-lg">
     <div class="row mt-3">
         <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded-3 bg-light p-5">
             <h1 class="display-3">Анализатор страниц</h1>
@@ -16,4 +19,5 @@
             </form>
         </div>
     </div>
+</div>
 @endsection
