360f25da618042a5808f47274ecfa957fc8325e3
Maga-Gazdiev <marmokanime@yandex.com>
2022-10-30 18:16:43 +0300

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,9 +17,10 @@
                 <div class="col-8">
                 <input type="text" name="url[name]" value="" class="form-control form-control-lg is-invalid" placeholder="https://www.example.com">
                 <div class="invalid-feedback">Некорректный URL</div>
+                </div>
+                <div>
                 <input type="submit" class="btn btn-primary btn-lg ms-3 px-5 text-uppercase mx-1" value="Проверить">
                 </div>
-               
             </form>
         </div>
     </div>
