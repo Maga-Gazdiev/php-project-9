3ae11924081eb3fbdd5c9cb225f04651d0bce7c8
Moker <marmokanime@yandex.com>
2022-11-01 13:54:15 +0300

    push

--- app/Http/Controllers/PrServer.php
+++ app/Http/Controllers/PrServer.php
@@ -30,7 +30,7 @@ class PrServer extends Controller
         $Url = $request->input('url.name');
 
         if ($validated->fails() && substr($Url, 0, 8) !== "https://" || $validated->fails() && substr($Url, 0, 7) !== "http://") {
-        header("Status: 402");
+        header("http://127.0.0.1:8000/urls", TRUE, 402);
         return view(view: "errorWelcome")->withErrors($validated);
         } else {  
         $getNormalUrl = function($Url)
